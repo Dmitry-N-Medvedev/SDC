3c2ecc7c0d1c9070687655eb3a04cc45830e6c82
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2021-01-09 12:30:40 +0100

    added PM2
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

---
+++ sources/Server/ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: 'sdc http server',
+      script: './index.mjs',
+      instances: 'max',
+      exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'development',
+        NODE_DEBUG: 'SDC_SERVER',
+        UWS_PORT: 9091,
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        NODE_DEBUG: 'SDC_SERVER',
+        UWS_PORT: 9091,
+      },
+      watch: '.',
+    },
+  ],
+};
