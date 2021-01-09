026983b1f1342ea0b3307bfbe69626cc6deb7eac
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2021-01-09 04:27:42 +0100

    added server
    1. added a test for a server process
    2. substituted console calls with debuglog
    3. extracted /direction handler to a separate function
    4. added AsyncLocalStorage for the /direction handler
    
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

---
+++ sources/Server/index.mjs
@@ -0,0 +1,52 @@
+import util from 'util';
+import {
+  config,
+} from 'dotenv';
+import {
+  LibServer,
+} from '@dmitry-n-medvedev/libserver/libServer.mjs';
+
+config();
+
+const LOG_ALL_DOMAIN = '';
+const debuglog = util.debuglog(process.env.SDC_SERVER ?? LOG_ALL_DOMAIN);
+const port = parseInt(process.env.UWS_PORT, 10);
+
+debuglog('process.env.SDC_SERVER:', process.env.SDC_SERVER);
+debuglog('process.env.UWS_PORT:', process.env.UWS_PORT);
+
+if (Number.isNaN(port) === true) {
+  debuglog('UWS_PORT environment variable is undefined. Exiting now.');
+
+  // eslint-disable-next-line no-process-exit
+  process.exit(0);
+}
+
+let libServer = null;
+
+const exitNow = async () => {
+  if (libServer !== null) {
+    await libServer.stop();
+
+    libServer = null;
+  }
+
+  // eslint-disable-next-line no-process-exit
+  process.exit(0);
+};
+
+process.once('uncaughtException', exitNow);
+process.once('unhandledRejection', exitNow);
+process.on('SIGINT', exitNow);
+process.on('SIGTERM', exitNow);
+process.on('SIGHUP', exitNow);
+
+(async () => {
+  libServer = new LibServer({
+    port,
+  });
+
+  await libServer.start();
+
+  debuglog(`started on ${port} port`);
+})();
