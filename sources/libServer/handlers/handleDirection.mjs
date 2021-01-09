da4efcc469e06bfb2529b4727227fe70c26e888c
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2021-01-09 11:57:51 +0100

    rm AsyncLocalStorage for now
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

--- sources/libServer/handlers/handleDirection.mjs
+++ sources/libServer/handlers/handleDirection.mjs
@@ -1,45 +1,27 @@
 import {
-  AsyncLocalStorage,
-} from 'async_hooks';
-import {
   calculateDirection,
 } from '@dmitry-n-medvedev/libdirectioncalculator';
 
-const als = new AsyncLocalStorage();
-
-const calc = async () => {
-  const heading = als.getStore().get('heading') ?? null;
-  const target = als.getStore().get('target') ?? null;
-
-  return calculateDirection(heading, target);
-};
-
 export const handleDirection = async (res, req) => {
-  als.run(new Map(), async () => {
-    res.aborted = false;
+  res.aborted = false;
 
-    res.onAborted(() => {
-      res.aborted = true;
-    });
-
-    try {
-      const heading = parseFloat(req.getQuery('heading')) ?? null;
-      const target = parseFloat(req.getQuery('target')) ?? null;
-
-      als.getStore()
-        .set('heading', heading)
-        .set('target', target);
-
-      if (res.aborted === false) {
-        res.end(JSON.stringify({
-          direction: (await calc()),
-        }));
-      }
-    } catch (anyError) {
-      if (res.aborted === false) {
-        res.writeStatus(`400 ${anyError.message}`);
-        res.end();
-      }
-    }
+  res.onAborted(() => {
+    res.aborted = true;
   });
+
+  const heading = parseFloat(req.getQuery('heading')) ?? null;
+  const target = parseFloat(req.getQuery('target')) ?? null;
+
+  try {
+    if (res.aborted === false) {
+      res.end(JSON.stringify({
+        direction: calculateDirection(heading, target),
+      }));
+    }
+  } catch (anyError) {
+    if (res.aborted === false) {
+      res.writeStatus(`400 ${anyError.message}`);
+      res.end();
+    }
+  }
 };
