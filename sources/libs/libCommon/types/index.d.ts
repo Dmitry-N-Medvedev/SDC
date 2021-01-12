9036e5d51939393f642c30e05db2770539f08c92
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2021-01-12 02:06:11 +0100

    WIP: trying to do manual types
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

---
+++ sources/libs/libCommon/types/index.d.ts
@@ -0,0 +1,14 @@
+declare namespace libCommon {
+  namespace constants {
+    export enum Degrees {
+      MIN_DEGREE = 0,
+      MAX_DEGREE = 359,
+    }
+
+    export enum Directions {
+      STRAIGHT = 'straight',
+      LEFT = 'left',
+      RIGHT = 'right',
+    }
+  }
+}
